b50abc39d1011351a49c7011c1aa64ef85ba5beb
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-07-29 20:32:44 -0400

    Create other.go
    Adding a json.Marshal and json.Unmarshal example with struct

---
+++ other.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "encoding/json"
+)
+
+type response struct {
+    SessionState string `json:"session_state"`
+    Scope        string `json:"scope"`
+}
+
+
+func main() {
+    data := &response {
+            SessionState: "__sessionState",
+            Scope: "__scope" }
+
+    jsonData, _ := json.Marshal(data)
+    fmt.Println("*data:", *data)
+    fmt.Println("jsonData:", jsonData)
+    fmt.Println("string(jsonData):", string(jsonData))
+
+    fmt.Println("===============================")
+
+    out := response{}
+    json.Unmarshal(jsonData, &out)
+    fmt.Println("out:", out)
+}
+
