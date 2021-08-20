8c54bc697bd8f725007fb4d33764a602f400fe57
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-08-20 16:45:27 -0400

    Update structs2.go

--- structs/structs2.go
+++ structs/structs2.go
@@ -1,1 +1,15 @@
+package main
 
+import	"fmt"
+
+type Customer struct {
+}
+
+func (Customer) Write(s string) {
+  fmt.Println("s:", s)
+}
+
+func main() {
+  cust := Customer{}
+  cust.Write("-message-")
+}
